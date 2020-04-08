e4fdcbcfa294b31a8b5a5d61dc56d84cbe5da437
Kento Kuga <kento.kuga.hidari@gmail.com>
2020-04-08 21:56:44 +0900

    first commit

---
+++ front/src/components/App.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import TextButton from "../ui/TextButton";
+
+interface props {}
+
+const App = (props: props) => {
+  return <TextButton text="Hello World" />;
+};
+
+export default App;
---
+++ front/src/ui/TextButton.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+
+interface props {
+  text: string;
+}
+
+const TextButton = (props: props) => {
+  return <Button> {props.text}</Button>;
+};
+
+export default TextButton;
