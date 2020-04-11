9e12a532b3582dbcbe822633dee35a656abdb644
Kento Kuga <kento.kuga.hidari@gmail.com>
2020-04-12 02:49:48 +0900

    スマートフォントUIを導入

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
+import "./core/scss/style.module.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Front from "./components/Front";
