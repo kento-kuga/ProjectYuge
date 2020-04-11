af71ef52cf88d1f991b922ec8a25e08a86370ac5
Kento Kuga <kento.kuga.hidari@gmail.com>
2020-04-11 18:36:00 +0900

    install scss

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,10 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import TextButton from "../ui/TextButton";
+import classes from "./App.module.scss";
 
 interface props {}
 
 const App = (props: props) => {
-  return <TextButton text="Hello World" />;
+  return (
+    <>
+      <div className={classes.AppButtom}>
+        <TextButton text="Hello World" />
+      </div>
+    </>
+  );
 };
 
 export default App;
