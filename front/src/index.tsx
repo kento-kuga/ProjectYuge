57e11fa3100cf2662bac1bc21ee7baac03ede99f
Kento Kuga <kento.kuga.hidari@gmail.com>
2020-04-18 23:50:20 +0900

    semantic uiのカスタムテーマを導入

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import "semantic-ui-css/semantic.min.css";
+import "./semantic/dist/semantic.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./core/css/style.scss";
 
